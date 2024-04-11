0acc03b72ca545228df1230e382cb9a46fa69139
Bownie <lukasdng@gmail.com>
2024-04-11 19:35:55 -0300

    npm install e start

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const http = require("http");
 
 const port = 3000;
 
-const server = http.createServer();
+const server = http.createServer ((request, response) => {
+    response.setHeader("content-type", "text/html; charset=utf-8");
+    response.writeHead(200);
+    response.write("Olá Node JS!");
+    response.end();
+});
 
 server.listen(3000, () => {
     console.log("Servidor iniciado na porta: ", port)
